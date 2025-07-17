0ba01a16255bc005e12d55fcfc6e1d5c5716927c
Muqeem Ishtiaq <muqeemishtiaq1234@gmail.com>
2025-07-17 17:12:01 +0500

    new

--- js/index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('faq-toggle').addEventListener('click', function() {
-  const faqContent = document.getElementById('faq-content');
-  const toggleIcon = this;
-  
-  faqContent.classList.toggle('hidden');
-  
-  toggleIcon.classList.toggle('scale-y-[-1]');
-});
